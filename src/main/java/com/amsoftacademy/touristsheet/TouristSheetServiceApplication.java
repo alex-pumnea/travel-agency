ab6faf07babf9ae0178d0263a0cb609e39204c95
Alex Pumnea <alexandr.pumnea@gmail.com>
2023-06-20 23:22:55 +0300

    (add) Project setup and basic structure

---
+++ src/main/java/com/amsoftacademy/touristsheet/TouristSheetServiceApplication.java
@@ -0,0 +1,13 @@
+package com.amsoftacademy.touristsheet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TouristSheetServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TouristSheetServiceApplication.class, args);
+	}
+
+}
