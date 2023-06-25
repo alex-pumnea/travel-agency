874522a64673383e605f2dfdaffcd949d864cccb
Alex Pumnea <alexandr.pumnea@gmail.com>
2023-06-25 18:36:31 +0300

    (add) Relationship enum with Javadoc
    Description:
    
    - Created the Relationship enum.
    - Added comprehensive Javadoc comments.

---
+++ src/main/java/com/amsoftacademy/touristsheet/domain/Relationship.java
@@ -0,0 +1,13 @@
+package com.amsoftacademy.touristsheet.domain;
+/**
+ * Relationship enum.
+ *
+ * @author Alex Pumnea
+ */
+public enum Relationship {
+        SPOUSE,
+        PARENT,
+        CHILD,
+        SIBLING,
+        OTHER
+    }
