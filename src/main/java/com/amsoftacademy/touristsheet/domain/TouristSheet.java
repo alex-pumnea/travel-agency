9c3a5616d36462593929e51909d739b15406b7ff
Alex Pumnea <alexandr.pumnea@gmail.com>
2023-06-25 19:33:54 +0300

    (add) TouristSheet class with constraints, table, and Javadoc
    Description:
    
     - Created the TouristSheet class to represent tourist sheet entity.
     - Applied constraints to the class attributes to enforce data validation.
     - Defined the database table mapping for the TouristSheet class using annotations.
     - Added comprehensive Javadoc comments.

---
+++ src/main/java/com/amsoftacademy/touristsheet/domain/TouristSheet.java
@@ -0,0 +1,62 @@
+package com.amsoftacademy.touristsheet.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.proxy.HibernateProxy;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.Objects;
+
+/**
+ * Represents a Tourist Sheet entity.
+ *
+ * @author Alex Pumnea
+ */
+@Entity
+@Table(name = "tourist_sheets")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class TouristSheet {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "address_id")
+    private Address address;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "travel_info_id")
+    private TravelInfo travelInfo;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "emergency_contact_id")
+    private EmergencyContact emergencyContact;
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        TouristSheet that = (TouristSheet) o;
+        return getId() != null && Objects.equals(getId(), that.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return getClass().hashCode();
+    }
+}
