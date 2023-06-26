b029e4af8d2d52bb93c0dbab89fd7f1d7a4f6401
Alex Pumnea <alexandr.pumnea@gmail.com>
2023-06-26 07:34:05 +0300

    (refactor) Reformat according to style guides
    Description:
    
     - Added an empty line after the package declaration

--- src/main/java/com/amsoftacademy/touristsheet/domain/Relationship.java
+++ src/main/java/com/amsoftacademy/touristsheet/domain/Relationship.java
@@ -1,13 +1,14 @@
 package com.amsoftacademy.touristsheet.domain;
+
 /**
  * Relationship enum.
  *
  * @author Alex Pumnea
  */
 public enum Relationship {
-        SPOUSE,
-        PARENT,
-        CHILD,
-        SIBLING,
-        OTHER
-    }
+    SPOUSE,
+    PARENT,
+    CHILD,
+    SIBLING,
+    OTHER
+}
