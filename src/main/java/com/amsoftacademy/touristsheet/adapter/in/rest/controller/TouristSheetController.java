8f81607ebb7f0e396f67d72328be444df446e152
Alex Pumnea <alexandr.pumnea@gmail.com>
2023-07-03 23:43:24 +0300

    (add) Controller to handle TouristSheet-related API endpoints
    Description:
    - Provide CRUD operations for TouristSheet via endpoints.
    - HTTP status code for each endpoint
    - Validation of payload

--- src/main/java/com/amsoftacademy/touristsheet/adapter/in/rest/controller/TouristSheetController.java
+++ src/main/java/com/amsoftacademy/touristsheet/adapter/in/rest/controller/TouristSheetController.java
@@ -1,59 +1,94 @@
 package com.amsoftacademy.touristsheet.adapter.in.rest.controller;
 
 import com.amsoftacademy.touristsheet.adapter.in.rest.dto.TouristSheetRequest;
-import com.amsoftacademy.touristsheet.application.TouristSheetService;
-import com.amsoftacademy.touristsheet.domain.TouristSheet;
+import com.amsoftacademy.touristsheet.adapter.in.rest.dto.TouristSheetResponse;
+import com.amsoftacademy.touristsheet.application.port.TouristService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.Optional;
 
 /**
+ * Controller for handling TouristSheet-related operations.
+ * Acts as an entry point for TouristSheet-related API endpoints.
+ *
  * @author Alex Pumnea
  */
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/tourist-sheets")
 public class TouristSheetController {
-    private final TouristSheetService<TouristSheetRequest, TouristSheet> service;
+    private final TouristService<TouristSheetRequest, TouristSheetResponse> touristService;
 
+    /**
+     * Retrieves all TouristSheets.
+     *
+     * @return ResponseEntity containing the iterable of TouristSheet
+     */
     @GetMapping
-    ResponseEntity<Iterable<TouristSheet>> getAll() {
-        return ResponseEntity.ok(service.getAll());
+    ResponseEntity<Iterable<TouristSheetResponse>> getAll() {
+        return ResponseEntity.ok(touristService.getAll());
     }
 
+    /**
+     * Retrieves a TouristSheet by ID.
+     *
+     * @param id the ID of the TouristSheet to retrieve
+     * @return ResponseEntity containing the TouristSheetResponse
+     */
     @GetMapping("/{id}")
-    ResponseEntity<Optional<TouristSheet>> getById(@PathVariable Long id) {
-        return ResponseEntity.ok(service.getById(id));
+    ResponseEntity<TouristSheetResponse> getById(@PathVariable Long id) {
+        return ResponseEntity.status(HttpStatus.OK).body(this.touristService.getById(id));
     }
 
-    //    TODO: Solve the issue of saving duplicates
+    /**
+     * Creates a new TouristSheet.
+     *
+     * @param request the TouristSheetRequest object containing the TouristSheet details
+     * @return ResponseEntity containing the created TouristSheetResponse
+     */
     @PostMapping
-    ResponseEntity<TouristSheet> save(@Valid @RequestBody TouristSheetRequest request) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(service.save(request));
+    ResponseEntity<TouristSheetResponse> save(@Valid @RequestBody TouristSheetRequest request) {
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(this.touristService.save(request));
     }
 
+    /**
+     * Updates an existing TouristSheet.
+     *
+     * @param id      the ID of the TouristSheet to update
+     * @param request the TouristSheetRequest object containing the updated TouristSheet details
+     * @return ResponseEntity containing the updated TouristSheetResponse
+     */
     @PutMapping("/{id}")
-    ResponseEntity<TouristSheet> updateById(@PathVariable Long id, @Valid @RequestBody TouristSheetRequest request) {
-        return ResponseEntity.accepted().body(service.update(id, request));
+    ResponseEntity<TouristSheetResponse> updateById(@PathVariable Long id, @Valid @RequestBody TouristSheetRequest request) {
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(this.touristService.update(id, request));
     }
 
-    @DeleteMapping()
-    ResponseEntity<TouristSheet> deleteAll() {
-        boolean allDeleted = service.removeAll();
-
-        return allDeleted
-                ? ResponseEntity.noContent().build()
-                : ResponseEntity.status(HttpStatus.NOT_MODIFIED).build();
-    }
-
+    /**
+     * Deletes a TouristSheet by ID.
+     *
+     * @param id the ID of the TouristSheet to delete
+     * @return ResponseEntity with no content
+     */
     @DeleteMapping("/{id}")
-    ResponseEntity<TouristSheet> deleteById(@PathVariable Long id) {
-        service.removeById(id);
+    ResponseEntity<TouristSheetResponse> deleteById(@PathVariable Long id) {
+        this.touristService.removeById(id);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
+    /**
+     * Deletes all TouristSheets.
+     *
+     * @return ResponseEntity with no content
+     */
+    @DeleteMapping()
+    ResponseEntity<TouristSheetResponse> deleteAll() {
+        this.touristService.removeAll();
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
 }
